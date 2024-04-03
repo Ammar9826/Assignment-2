52f7578b8eb55bda8d317a692bc7ec9994ba95b2
Muhammad Ammar <165484538+Ammar9826@users.noreply.github.com>
2024-04-03 09:32:08 -0400

    Add files via upload

---
+++ Assignment 2 (Data World).py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Task 01:
+# 
+# Print Your Name with your Father name and Date of birth using suitable escape sequence charactor
+# 
+# 
+
+# In[1]:
+
+
+name = "Muhammad Ammar"
+father_name = "Muhammad Amin"
+dob = "02/26/1998"
+
+print("Name: {}\nFather's Name: {}\nDate of Birth: {}".format(name, father_name, dob))
+
+
+# # Task 02:
+# 
+# Write your small bio using variables and print it using print function
+
+# In[2]:
+
+
+bio = "My name is Muhammad Ammar. I currently lived in United States. Pursuing degree in Data Science. Studied in cybersecurity."
+print(bio)
+
+
+# # Task 03:
+# 
+# Write a program in which use all the operators we can use in Python
+
+# In[3]:
+
+
+a = 10
+b = 20
+
+# Arithmetic operators
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
+print("Modulus: ", a % b)
+print("Exponentiation: ", a ** b)
+print("Floor division: ", a // b)
+
+# Comparison operators
+print("Equal: ", a == b)
+print("Not equal: ", a != b)
+print("Greater than: ", a > b)
+print("Less than: ", a < b)
+print("Greater than or equal to: ", a >= b)
+print("Less than or equal to: ", a <= b)
+
+# Logical operators
+print("AND: ", a > 5 and b > 5)
+print("OR: ", a > 5 or b > 5)
+print("NOT: ", not(a > 5 and b > 5))
+
+
+# # Task 04:
+# 
+# 
+# 1. Mention Marks of English , Islamiat and Maths out of 100 in 3 different variables
+# 
+# 2. Mention Variable of Total Marks and assign 300 to it
+# 
+# 3. Calculate Percentage
+
+# In[4]:
+
+
+english_marks = 92
+islamiat_marks = 95
+maths_marks = 90
+
+total_marks = 300
+
+percentage = ((english_marks + islamiat_marks + maths_marks) / total_marks) * 100
+print("Percentage: ", percentage)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
